183d698a856cf3523cd54c90838dfa8f201265c3
dilisha-kalyani <87657229+dilisha-kalyani@users.noreply.github.com>
2021-08-30 23:47:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,126 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+var ball;
+var ground;
+var con;
+var ball2;
+var con2;
+var ball3, con3;
+var ball4, con4;
+var ball5, con5;
+
+var rope;
+
+
+ function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  
+  world = engine.world;
+
+  var ball_options = {
+    restitution: 0.8
+  }
+  
+  
+  ball = Bodies.circle(200,50,10,ball_options);
+  World.add(world,ball);
+
+  ball2 = Bodies.circle(350,10,12,ball_options);
+  World.add(world,ball2);
+  
+  ball3 = Bodies.circle(250,50,10,ball_options);
+  World.add(world,ball);
+
+  ball4 = Bodies.circle(400,50,10,ball_options);
+  World.add(world,ball);
+
+  ball5 = Bodies.circle(450,50,10,ball_options);
+  World.add(world,ball);
+
+  rope = Bodies.circle(350,10,12,0);
+  World.add(world,rope);
+
+  con = Matter.Constraint.create({
+          pointA:{x:200,y:20},
+          bodyB:ball,
+          pointB:{x:0,y:0},
+          length:100,
+          stiffness:0.1
+        });
+      World.add(world,con);
+      
+      con3 = Matter.Constraint.create({
+        pointA:{x:200,y:20},
+        bodyB:ball,
+        pointB:{x:0,y:0},
+        length:100,
+        stiffness:0.1
+      });
+    World.add(world,con3);
+
+    con4 = Matter.Constraint.create({
+      pointA:{x:200,y:20},
+      bodyB:ball,
+      pointB:{x:0,y:0},
+      length:100,
+      stiffness:0.1
+    });
+  World.add(world,con4);
+
+  con5= Matter.Constraint.create({
+    pointA:{x:200,y:20},
+    bodyB:ball,
+    pointB:{x:0,y:0},
+    length:100,
+    stiffness:0.1
+  });
+World.add(world,con5);
+  
+        
+   con2 = Matter.Constraint.create({
+          bodyA:ball,
+          pointA:{x:0,y:0},
+          bodyB:ball2,
+          pointB:{x:0,y:0},
+          length:100,
+          stiffness:0.1
+        });
+  
+      World.add(world,con2);
+  
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  
+}
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  ellipse(ball.position.x,ball.position.y,10);
+  ellipse(ball2.position.x,ball2.position.y,12);
+  ellipse(ball3.position.x,ball2.position.y,14);
+  ellipse(ball4.position.x,ball2.position.y,16);
+  ellipse(ball5.position.x,ball2.position.y,18);
+
+  push();
+  strokeWeight(2);
+  stroke(255);
+  line(con.pointA.x,con.pointA.y,ball.position.x,ball.position.y);
+  line(ball.position.x,ball.position.y,ball2.position.x,ball2.position.y);
+  pop();
+  
+}
+
+function keyPressed()
+{
+  if(keyCode==RIGHT_ARROW)
+    {
+      Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+    }
+}
